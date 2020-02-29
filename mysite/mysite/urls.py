8561cc5310c62d5eadd695a7d6b6a7d9a67e684d
wonjun <dnjswns2280@naver.com>
2020-03-01 01:17:12 +0900

    Initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,24 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import include, url
+
+urlpatterns = [
+    # url 패턴을 포함시킬 때에는 항상 include를 사용해야함
+    url(r'^polls/', include('polls.urls')),
+    # admin만 예외
+    url(r'^admin/', admin.site.urls),
+]
---
+++ mysite/polls/views.py
@@ -0,0 +1,42 @@
+# Create your views here.
+# 제네릭 뷰 사용
+
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 에러 메세지와 함께 폼을 다시 디스플레이합니다.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터 처리를 정상적으로 마친 뒤에는 항상 HttpResponseRedirect를 리턴합니다.
+        # 이 방법을 통해 유저가 브라우저의 "뒤로가기"을 눌렀을 때
+        # 데이터가 두 번 저장되는 것을 방지할 수 있습니다.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
